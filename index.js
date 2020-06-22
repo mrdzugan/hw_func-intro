7bb477a4a1ddcb85fed4eb453c7cadd07f235abd
Andrei Dzugan <mr.dzugan@gmail.com>
2020-06-22 10:17:48 +0300

    Add checkMultiplicity(a, b) function
    The function checks if the first number is a multiple of the second

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ function isAdult(num) {
 	num = Number(num);
 	return num >= 18;
 }
+
+console.log(checkMultiplicity(15, 4));
+
+/**
+ *
+ * @param a {int}
+ * @param b {int}
+ * @returns {boolean}
+ */
+function checkMultiplicity(a, b) {
+	const num1 = Number(a);
+	const num2 = Number(b);
+	return !(num1 % num2);
+}
