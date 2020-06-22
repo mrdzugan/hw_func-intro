d80c687e300d292c41b4b539cbdf6e04e40a3cfd
Andrei Dzugan <mr.dzugan@gmail.com>
2020-06-22 11:14:36 +0300

    Add square calculation functions
    Square functions of: triangle, rhomb, cylinder, rectangle

--- index.js
+++ index.js
@@ -39,3 +39,59 @@ function checkTriangle(a, b, c) {
 	const side3 = Number(c);
 	return side1 + side2 > side3;
 }
+
+console.log(rhombSquare(5, 7));
+
+/**
+ *
+ * @param a {int}
+ * @param h {int}
+ * @returns {number}
+ */
+function rhombSquare(a, h) {
+	const side = Number(a);
+	const height = Number(h);
+	return side * height;
+}
+
+console.log(cylinderSquare(5, 6));
+
+/**
+ *
+ * @param r {int}
+ * @param h {int}
+ * @returns {number}
+ */
+function cylinderSquare(r, h) {
+	const radius = Number(r);
+	const height = Number(h);
+	return 2 * Math.PI * radius * height;
+}
+
+console.log(triangleSquare(5, 10));
+
+/**
+ *
+ * @param a {int}
+ * @param h {int}
+ * @returns {number}
+ */
+function triangleSquare(a, h) {
+	const side = Number(a);
+	const height = Number(h);
+	return side * height / 2;
+}
+
+console.log(rectangleSquare(5, 3));
+
+/**
+ *
+ * @param a {int}
+ * @param b {int}
+ * @returns {number}
+ */
+function rectangleSquare(a, b) {
+	const side1 = Number(a);
+	const side2 = Number(b);
+	return side1 * side2;
+}
