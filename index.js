f77c0e19f8a573df8f65cdd7e3d9c639703f1bd8
Andrei Dzugan <mr.dzugan@gmail.com>
2020-06-22 10:26:47 +0300

    Add checkTriangle() function
    The function checks is a triangle possible

--- index.js
+++ index.js
@@ -23,3 +23,19 @@ function checkMultiplicity(a, b) {
 	const num2 = Number(b);
 	return !(num1 % num2);
 }
+
+console.log(checkTriangle(2, 5, 9));
+
+/**
+ *
+ * @param a {int}
+ * @param b {int}
+ * @param c {int}
+ * @returns {boolean}
+ */
+function checkTriangle(a, b, c) {
+	const side1 = Number(a);
+	const side2 = Number(b);
+	const side3 = Number(c);
+	return side1 + side2 > side3;
+}
